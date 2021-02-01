53a9f6f50bd4c2307ac56b54e49efaa8c227d449
milanajmerainfotech <milanajmerainfotech>
2021-02-01 18:11:03 +0530

    commited

--- app/src/main/java/com/example/demofornew/MainActivity.java
+++ app/src/main/java/com/example/demofornew/MainActivity.java
@@ -2,25 +2,83 @@ package com.example.demofornew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.android.demoliabrery.CustomToast;
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
 
 public class MainActivity extends AppCompatActivity {
+    public Socket socket;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            socket = IO.socket("http://chat.socket.io");
+            Toast.makeText(this, "User Connected To Socket IO", Toast.LENGTH_SHORT).show();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+
+        socket.connect();
+
+
         Button button = findViewById(R.id.button_clic);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CustomToast.createToast(MainActivity.this, "Call From Click");
+                socket.on("message", onNewMessage);
+                socket.emit("message", "New Message Sent.");
             }
         });
+    }
+    Activity context = MainActivity.this;
+    public Emitter.Listener onNewMessage = args -> {
+        Log.d("ExceptionMilan", "Cliled");
+        context.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                JSONObject data = (JSONObject) args[0];
+                String username;
+                String message;
+                try {
+                    username = data.getString("username");
+                    message = data.getString("message");
+                    CustomToast.createToast(MainActivity.this, message);
+                } catch (JSONException e) {
+                    Log.d("ExceptionMilan", e.toString());
+                    return;
+                }
+
+                // add the message to view
+                //addMessage(username, message);
+                Toast.makeText(MainActivity.this, message + " (Received)", Toast.LENGTH_SHORT).show();
+            }
+        });
+    };
+
+    @Override
+    protected void onDestroy() {
+        socket.disconnect();
+        socket.off("message", onNewMessage);
+        Toast.makeText(this, "User Disconnected To Socket IO", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
     }
 }
--- demoliabrery/src/main/java/com/android/demoliabrery/CustomToast.java
+++ demoliabrery/src/main/java/com/android/demoliabrery/CustomToast.java
@@ -3,34 +3,37 @@ package com.android.demoliabrery;
 import android.content.Context;
 import android.os.Handler;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class CustomToast {
     public static final int CUSTOM_TOAST_LAYOUT = R.layout.toast_layout;
 
     static View view;
+    public static View setCustomeToastLayout(Context context, CharSequence text){
+        View view = LayoutInflater.from(context).inflate(R.layout.toast_layout,null);
+        TextView textV = (TextView) view.findViewById(R.id.text_toast);
+        textV.setText(text);
+
+        Toast mToast = new Toast(context);
+        mToast.setView(view);
+        mToast.setGravity(Gravity.BOTTOM, 0, 256);
+        mToast.setDuration(Toast.LENGTH_LONG);
+        mToast.show();
+        return view;
+    }
 
     public static void createToast(Context context, CharSequence text) {
         view = setCustomeToastLayout(context, text);
-    }
-
-    public static View setCustomeToastLayout(Context context, CharSequence text){
-        Log.d("MilanChecking", "Comes Fomr Class");
-        View view = LayoutInflater.from(context).inflate(CUSTOM_TOAST_LAYOUT, null);
-        TextView textView = view.findViewById(R.id.text_toast);
-        textView.setText(text);
-        view.setVisibility(View.VISIBLE);
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                view.setVisibility(View.GONE);
-            }
-        },1000);
-
-        return view;
     }
 
     public void showCustomToast(){
